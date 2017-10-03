60066c39006bd9f51fef79dce6e641d5cc028875
Matthew Wagar <mrw77@drexel.edu>
2017-10-03 15:51:25 -0400

    Started the UI with ember paper

---
+++ app/router.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+const Router = Ember.Router.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('home', {
+    path: '/'
+  }, function() {
+    this.route('about');
+    this.route('team');
+    this.route('blog');
+    this.route('play');
+  });
+});
+
+export default Router;
