725ba3fe0eabfc98c7a5e0a6eaade285325ccdbc
Matthew Wagar <mrw77@drexel.edu>
2017-12-14 08:09:21 -0500

    Added all the presentations to the documentation page

--- app/controllers/app/documentation.js
+++ app/controllers/app/documentation.js
@@ -2,4 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     selectedDoc:"gdd",
+
+    selectedSell:"sell1",
+    selectedScrum:"scrum10",
+
+    actions:{
+        mutate(obj, key){
+            this.set(obj, key);
+        }
+    }
 });
